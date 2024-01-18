f8cdced2f22048c14d9361d6b6af38d9f835ad4f
Ankit Kumar <ankitkumar95993@gmail.com>
2024-01-18 22:28:48 +0530

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,16 @@
 
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+mongoose.connect(process.env.MONGODB_URL).then(()=>console.log('db connection successful'))
+.catch((error)=>{
+    console.log('db connection not successful');
+    console.error(error);
+    process.exit(1);
+});
+
 const app = express();
+// app.use(express.json());
 
 app.listen(3000,()=>{
     console.log('app is running at port 3000');
