ef84016cd4b6b537a573629421050d9b65dce320
Ankit Kumar <ankitkumar95993@gmail.com>
2024-01-23 22:41:44 +0530

    imported user in header file

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,13 +1,16 @@
 import React from "react";
-import { Navbar, TextInput, Button } from "flowbite-react";
+import { Navbar, TextInput, Button, Dropdown, Avatar } from "flowbite-react";
 import { Link } from "react-router-dom";
 // import { useState } from 'react';
 import { IoIosSearch } from "react-icons/io";
 import { IoMoonSharp } from "react-icons/io5";
-import {useLocation} from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function Header() {
+  const { currentUser } = useSelector((state) => state.user);
   const path = useLocation().pathname;
+
   return (
     <Navbar className="border-b-2">
       <Link
@@ -31,31 +34,53 @@ export default function Header() {
       <Button className="w-12 h-10 lg:hidden" color="gray" pill>
         <IoIosSearch />
       </Button>
-      
+
       <div className="flex gap-2 md:order-2">
         <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
           <IoMoonSharp />
         </Button>
-        <Link to="/sign-in">
-          <Button gradientDuoTone="purpleToPink" outline>
-            Sign in
-          </Button>
-        </Link>
-        <Navbar.Toggle/>
-        </div>
 
-        <Navbar.Collapse>
-          <Navbar.Link active={path==='/'} as={'div'}>
-            <Link to='/'>Home</Link>
-          </Navbar.Link>
-          <Navbar.Link active={path==='/about'} as={'div'}>
-            <Link to='/about'>About</Link>
-          </Navbar.Link>
-          <Navbar.Link active={path==='/projects'} as={'div'}>
-            <Link to='/projects'>Projects</Link>
-          </Navbar.Link>
+        {currentUser ? (
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar alt="user" img={currentUser.profilePicture} rounded />
+            }
+          >
+            <Dropdown.Header>
+             <span>{currentUser.username}</span>
+             <span>{currentUser.email}</span>
 
-        </Navbar.Collapse>
+              
+
+            </Dropdown.Header>
+
+
+
+          </Dropdown>
+        ) : (
+          <Link to="/sign-in">
+            <Button gradientDuoTone="purpleToPink" outline>
+              Sign in
+            </Button>
+          </Link>
+        )}
+
+        <Navbar.Toggle />
+      </div>
+
+      <Navbar.Collapse>
+        <Navbar.Link active={path === "/"} as={"div"}>
+          <Link to="/">Home</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/about"} as={"div"}>
+          <Link to="/about">About</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/projects"} as={"div"}>
+          <Link to="/projects">Projects</Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
     </Navbar>
   );
 }
