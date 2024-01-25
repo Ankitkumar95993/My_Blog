a4ed9981c344d2fee86a6f4a564a869017ed0faa
Ankit Kumar <ankitkumar95993@gmail.com>
2024-01-25 21:40:00 +0530

    complete profile page UI

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -45,7 +45,7 @@ export default function Header() {
           pill
           onClick={() => dispatch(toggleTheme())}
         >
-          {theme === "light" ? <FaSun /> : <FaMoon />}
+          {theme === "light" ? <FaMoon />: <FaSun /> }
         </Button>
 
         {currentUser ? (
