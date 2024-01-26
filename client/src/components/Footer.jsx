f61d6d95018bd55919360701039f07d9280a2cae
Ankit Kumar <ankitkumar95993@gmail.com>
2024-01-26 09:59:22 +0530

    compler upload image functionality

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,14 +1,18 @@
 import { Footer } from "flowbite-react";
 import { Link } from "react-router-dom";
-import { BsGithub,BsTwitter,BsFacebook,BsInstagram,BsLinkedin } from "react-icons/bs";
+import {
+  BsGithub,
+  BsTwitter,
+  BsFacebook,
+  BsInstagram,
+  BsLinkedin,
+} from "react-icons/bs";
 
 export default function FooterCom() {
   return (
     <Footer container className="border border-t-8 border-teal-500">
       <div className="w-full max-w-7xl mx-auto ">
-
         <div className="grid w-full justify-between sm:flex md:grid-cols-1">
-
           <div className="mt-5">
             <Link
               to="/"
@@ -83,31 +87,24 @@ export default function FooterCom() {
               </Footer.LinkGroup>
             </div>
           </div>
-
-
-          
-
         </div>
-
-     < Footer.Divider />
-       <div className=" w-full sm:flex sm:items-center sm:justify-between">
-        <Footer.Copyright 
-        href="#" 
-        by="Ankit's Blog"
-        year={new Date().getFullYear()}
-        />
-         <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
+                                         
+        <Footer.Divider />
+        <div className=" w-full sm:flex sm:items-center sm:justify-between">
+          <Footer.Copyright
+            href="#"
+            by="Ankit's Blog"
+            year={new Date().getFullYear()}
+          />
+          <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
             <Footer.Icon href="#" icon={BsFacebook} />
             <Footer.Icon href="#" icon={BsInstagram} />
             <Footer.Icon href="#" icon={BsTwitter} />
             <Footer.Icon href="#" icon={BsGithub} />
             <Footer.Icon href="#" icon={BsLinkedin} />
-
-
-        </div> 
+          </div>
+        </div>
       </div>
-     </div>
-      
     </Footer>
   );
 }
--- client/src/components/ThemeProvider.jsx
+++ client/src/components/ThemeProvider.jsx
@@ -10,7 +10,7 @@ const {theme} = useSelector((state)=>state.theme);
         <div className='bg-white text-gray-700 dark:text-gray-200 dark:bg-[rgb(16,23,42)] min-h-screen' >
             {children}
         </div>
-    // </div>
+     </div>
    
   )
 }
