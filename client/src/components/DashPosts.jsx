ffff9ca7c01c1929c52e3b757dd80e44d94d585f
Ankit Kumar <ankitkumar95993@gmail.com>
2024-03-13 12:20:36 +0530

    Added dashboard section to the side bar

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -15,7 +15,7 @@ export default function DashPosts() {
   useEffect(() => {
     const fetchPosts = async () => {
       try {
-        const res = await fetch(`/api/post/getPosts?${currentUser._id}`);
+        const res = await fetch(`/api/post/getposts?userId=${currentUser._id}`);
         const data = await res.json();
         if (res.ok) {
           setUserPosts(data.posts);
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import DashboardComp from "../components/DashboardComp";
 import DashComments from "../components/DashComments";
 import DashPosts from "../components/DashPosts";
 import DashProfile from "../components/DashProfile";
@@ -30,6 +31,7 @@ export default function Dashboard() {
       {tab==='posts' && <DashPosts/>}
       {tab==='users' && <DashUsers/>} 
       {tab==='comments' && <DashComments/>}
+      {tab==='dashboard' && <DashboardComp/>}
     </div>
   );
 }
