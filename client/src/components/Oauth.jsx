49398ce5013df1fe36c5ee13911e180586de359d
Ankit Kumar <ankitkumar95993@gmail.com>
2024-08-03 22:30:57 +0530

    firebase edited

--- client/src/components/Oauth.jsx
+++ client/src/components/Oauth.jsx
@@ -1,54 +1,47 @@
 import React from "react";
-import { Button } from "flowbite-react";
-import { AiFillGoogleCircle } from "react-icons/ai";
-import { GoogleAuthProvider, signInWithPopup, getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { app } from "../firebase";
-import { signInSuccess } from "../redux/user/userSlice";
-import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
+import { signInSuccess } from "../redux/user/userSlice";
+import {useNavigate} from "react-router-dom";
 
-export default function Oauth() {
-  const navigate = useNavigate();
+export default function OAuth() {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const handleGoogleClick = async () => {
-    const auth = getAuth(app);
-    const provider = new GoogleAuthProvider();
-    provider.setCustomParameters({ prompt: "select_account" });
     try {
-      const resultFromGoogle = await signInWithPopup(auth, provider);
-      //   console.log(resultFromGoogle);
-      const res = await fetch("/api/auth/google", {
+      const provider = new GoogleAuthProvider();
+      const auth = getAuth(app);
+
+      const result = await signInWithPopup(auth, provider);
+      console.log(result);
+      const res = await fetch("/api/v1/auth/google", {
         method: "POST",
         headers: {
-          "Content-Type": "application/json",
+          "content-type": "application/json",
         },
         body: JSON.stringify({
-          name: resultFromGoogle.user.displayName,
-          email: resultFromGoogle.user.email,
-          googlePhotoUrl: resultFromGoogle.user.photoURL,
+          name: result.user.displayName,
+          email: result.user.email,
+          photo: result.user.photoURL,
         }),
       });
       const data = await res.json();
-      if (res.ok) {
-        console.log(data);
-        dispatch(signInSuccess(data));
-        navigate("/");
-      }
+      dispatch(signInSuccess(data));
+      navigate('/');
     } catch (error) {
-      console.log(error);
+      console.log("could not sign in with google", error);
     }
   };
 
   return (
-    <Button
-      type="button"
-      gradientDuoTone="pinkToOrange"
-      outline
+    <button
       onClick={handleGoogleClick}
+      type="button"
+      className="p-3 bg-red-700 text-white rounded-lg uppercase hover:opacity-95"
     >
-      <AiFillGoogleCircle className="w-6 h-6 mr-2" />
-      Continue With Google
-    </Button>
+      Continue with google
+    </button>
   );
 }
--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,16 +1,13 @@
 
-
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAljKgSuFFEwxxLZG_BzaLrfbyUVF52Vbw",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "myblog-1349b.firebaseapp.com",
   projectId: "myblog-1349b",
   storageBucket: "myblog-1349b.appspot.com",
@@ -22,6 +19,6 @@ const firebaseConfig = {
 // Initialize Firebase
 // const app = initializeApp(firebaseConfig);
 export const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 
-//Initialize Firebase
+
